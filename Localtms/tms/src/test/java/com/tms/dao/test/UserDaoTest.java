6a9198f3c29bdc9be5cd3db7e09214f362d38d27
masternkv <niranjanverma1063@gmail.com>
2018-06-30 13:15:39 +0530

    In this commit: TestSuite Class contain all the classses to be in test New package created inside test folder that has the UserTestDao

---
+++ Localtms/tms/src/test/java/com/tms/dao/test/UserDaoTest.java
@@ -0,0 +1,5 @@
+package com.tms.dao.test;
+
+public class UserDaoTest {
+
+}
--- Localtms/tms/src/test/java/com/tms/test/TestSuite.java
+++ Localtms/tms/src/test/java/com/tms/test/TestSuite.java
@@ -1,5 +1,8 @@
 package com.tms.test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.hibernate.SessionFactory;
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -8,6 +11,7 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.InjectMocks;
 import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.junit.Assert.*;
@@ -18,20 +22,23 @@ import com.tms.dao.UserDao;
 import com.tms.model.User;
 import com.tms.service.CustomUserDetailsService;
 import com.tms.service.UserService;
+import com.tms.service.UserServiceImpl;
 
 //@RunWith()
 public class TestSuite {
+	
 	@Mock
-	 private UserService userService;
+	UserDao userDao;
 	@InjectMocks
-	 private CustomUserDetailsService customUserDetailsService;
-	//private static User user;
-
+	UserServiceImpl userServiceImpl;
+	@Spy
+    List<User> users = new ArrayList<User>();
+	       
 @Before
 	public  void setMock()
 	{
 		MockitoAnnotations.initMocks(this);
-		
+		users=getUserList();
 	}
 	
 	
@@ -40,22 +47,36 @@ public class TestSuite {
 	{
 		
 		
-		assertNotNull(userService);
-		assertNotNull(customUserDetailsService);
+		assertNotNull(userDao);
+		assertNotNull(userServiceImpl);
 		//assertNotNull(user);
 	}
 	
 	@Test
 	public void testFindById()
 	{
-		//String a="bill";
-		//User user=userDao.findBySSO(a);
-		
-	    //when(userDao.findBySSO(a)).thenReturn(user);
-	    
-	   // assertEquals("bill",user.getFirstName());
+		User user=users.get(0);
+		int i=100;
+     when(userDao.findById(i)).thenReturn(user);
+     System.out.print("UserDao return value"+userDao.findById(anyInt()));
+	 assertEquals(userServiceImpl.findById(user.getId()),user);
+	 System.out.print(""+userServiceImpl.findById(user.getId()));
 	}
-	
+	public List<User> getUserList(){
+        User user1 = new User();
+        user1.setId(100);
+        user1.setSsoId("bill");
+        user1.setPassword("abc123");
+        user1.setFirstName("Bill");
+        user1.setLastName("Watcher");
+        user1.setEmail("bill@xyz.com");
+        user1.setState("Active");
+       
+         users.add(user1);
+        
+        //employees.add(e2);
+        return users;
+    }
 	
 	
 }
